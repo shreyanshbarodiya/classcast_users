3c7ed812e9594155194ad5831e66877b3f3963be
shreyansh <shreyansh.barodiya@gmail.com>
2018-06-19 16:24:30 +0530

    phone number

--- views.py
+++ views.py
@@ -18,65 +18,65 @@ def updateprofile(request):
 	if(request.method == "GET"):
 		return JsonResponse({'status': 'False', 'message': 'Get request'})
 
-	try:
+	# try:
 
 		# student_id = request.user.id
 		# username = request.POST.get('username')
-		student_id = int(request.POST.get('student_id'))
-		
-		first_name = request.POST.get('first_name')
-		last_name = request.POST.get('last_name')
-		
-		pincode = int(request.POST.get('pincode'))
-		standard = int(request.POST.get('standard'))
-		stream = request.POST.get('stream')
-		marks_scored_in_last_degree = float(request.POST.get('marks_scored_in_last_degree'))
-		DOB = float(request.POST.get('DOB'))
-		phone_number = request.POST.get('phone_number') 
-		
-		gender = request.POST.get('gender')
+	student_id = int(request.POST.get('student_id'))
 	
-
-
-		if Classcast_student_info.objects.filter(student_id=student_id).exists():
-			student_info = Classcast_student_info.objects.get(student_id=student_id)
-		else:
-			student_info = Classcast_student_info(student_id=student_id, 
-				total_time_spent_on_platform=0.0,
-				active_status='active', last_active=datetime.now() ,
-				total_karma_points=0.0)
-
-		user = User.objects.get(id=student_id)
-		user_profile = UserProfile.objects.get(user_id=student_id)
-
-		if first_name is not None:
-			user.first_name = first_name
-		if last_name is not None:
-			user.last_name = last_name
-		if (first_name is not None) and (last_name is not None):
-			user_profile.name = first_name + ' ' + last_name
-		if pincode is not None:
-			student_info.pincode = pincode
-		if standard is not None:
-			student_info.standard = standard	
-		if stream is not None:
-			student_info.stream = stream
-		if marks_scored_in_last_degree is not None:
-			student_info.marks_scored_in_last_degree = marks_scored_in_last_degree
-		if DOB is not None:
-			student_info.DOB = DOB
-		if phone_number is not None:
-			student_info.phone_number = phone_number
-		if gender is not None:
-			user_profile.gender = gender
+	first_name = request.POST.get('first_name')
+	last_name = request.POST.get('last_name')
+	
+	pincode = int(request.POST.get('pincode'))
+	standard = int(request.POST.get('standard'))
+	stream = request.POST.get('stream')
+	marks_scored_in_last_degree = float(request.POST.get('marks_scored_in_last_degree'))
+	DOB = float(request.POST.get('DOB'))
+	phone_number = request.POST.get('phone_number') 
+	
+	gender = request.POST.get('gender')
 
 
 
-		student_info.save()
-		user.save()
-		user_profile.save()
+	if Classcast_student_info.objects.filter(student_id=student_id).exists():
+		student_info = Classcast_student_info.objects.get(student_id=student_id)
+	else:
+		student_info = Classcast_student_info(student_id=student_id, 
+			total_time_spent_on_platform=0.0,
+			active_status='active', last_active=datetime.now() ,
+			total_karma_points=0.0)
 
-		return JsonResponse({'status': 'True', 'message': 'Successfully created/updated'})
+	user = User.objects.get(id=student_id)
+	user_profile = UserProfile.objects.get(user_id=student_id)
 
-	except Exception, e:
-		return JsonResponse({'status': 'False', 'message': 'Database error'})
+	if first_name is not None:
+		user.first_name = first_name
+	if last_name is not None:
+		user.last_name = last_name
+	if (first_name is not None) and (last_name is not None):
+		user_profile.name = first_name + ' ' + last_name
+	if pincode is not None:
+		student_info.pincode = pincode
+	if standard is not None:
+		student_info.standard = standard	
+	if stream is not None:
+		student_info.stream = stream
+	if marks_scored_in_last_degree is not None:
+		student_info.marks_scored_in_last_degree = marks_scored_in_last_degree
+	if DOB is not None:
+		student_info.DOB = DOB
+	if phone_number is not None:
+		student_info.phone_number = phone_number
+	if gender is not None:
+		user_profile.gender = gender
+
+
+
+	student_info.save()
+	user.save()
+	user_profile.save()
+
+	return JsonResponse({'status': 'True', 'message': 'Successfully created/updated'})
+
+	# except Exception, e:
+	# 	return JsonResponse({'status': 'False', 'message': 'Database error'})
